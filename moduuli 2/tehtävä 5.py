4ba613e46b591f430dac3996f75f8b27e5114f88
elliriskala <elli.riskala@hotmail.com>
2023-08-22 15:21:04 +0300

    Tehtävät 1-6

---
+++ moduuli 2/tehtävä 5.py
@@ -0,0 +1,22 @@
+# yksi leiviskä = 20 naulaa
+# yksi naula = 32 luotia
+# yksi luoti = 13.3g
+
+luoti = 13.3
+naula = 32 * luoti
+leiviskä = 20 * naula
+
+# print(f"{32 * luoti}")
+# print(f"{20 * naula}")
+
+num1 = int(input("Anna leiviskät: "))
+num2 = int(input("Anna naulat: "))
+num3 = float(input("Anna luodit: "))
+
+result = num1 * leiviskä + num2 * naula + num3 * luoti
+kg = int(result // 1000)
+g = result % 1000
+
+print(f"Kokonaismassa nykymittojen mukaan: {(num1 * leiviskä + num2 * naula + num3 * luoti)/1000}")
+print(f"Massa nykymittojen mukaan kg: {kg}")
+print(f"Massa nykymittojen mukaan g: {g:.3f}")
