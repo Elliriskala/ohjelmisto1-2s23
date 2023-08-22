92064c0981565155591779cacfe96c13dd3c10d1
elliriskala <elli.riskala@hotmail.com>
2023-08-22 14:16:55 +0300

    Tehtävät 1-6

---
+++ moduuli 2/tehtävä 4.py
@@ -0,0 +1,8 @@
+num1 = int(input("Kirjoita yksi kokonaisluku: "))
+num2 = int(input("Kirjoita toinen kokonaisluku: "))
+num3 = int(input("Kirjoita kolmas kokonaisluku: "))
+# lasketaan kokonaislukujen summa, tulo ja keskiarvo
+print(f"Lukujen summa: {(num1 + num2 + num3)}")
+print(f"Lukujen tulo: {(num1 * num2 * num3)}")
+print(f"Lukujen keskiarvo: {((num1 + num2 + num3)/3)}")
+
