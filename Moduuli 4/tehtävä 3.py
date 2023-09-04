914ce763957b5df9cc65b89b235127ce52d8bb30
elliriskala <elli.riskala@hotmail.com>
2023-09-04 13:05:15 +0300

    Moduuli 4

--- Moduuli 4/tehtävä 3.py
+++ Moduuli 4/tehtävä 3.py
@@ -1,12 +1,22 @@
+smallest = 0
+largest = 0
+user_input = input("Syötä luku tai 'Enter' lopettaaksesi: ")
+n = user_input
 
-n = 0
+if user_input != '':
+    n = float(user_input)
+    smallest = n
+    largest = n
+while user_input != '':
+    user_input = input("Syötä luku tai 'Enter' lopettaaksesi: ")
+    if user_input == '':
+        break
+    n = float(user_input)
+    if n <= smallest:
+        smallest = n
+    elif n >= largest:
+        largest = n
 
-while n != '':
-    int(n)
-    n = input("Syötä luku tai 'Enter' lopettaaksesi: ")
 print("Syötit 'enter' ja lukujen syöttäminen päättyi. ")
-
-
-
-
-
+print(f"Pienin syöttämäsi luku: {smallest}")
+print(f"Suurin syöttämäsi luku: {largest}")
