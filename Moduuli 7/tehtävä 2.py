f6f37a97294301502b440543a5487a760aa3d491
elliriskala <elli.riskala@hotmail.com>
2023-09-12 19:37:09 +0300

    Moduuli 7

---
+++ Moduuli 7/tehtävä 2.py
@@ -0,0 +1,25 @@
+# Kirjoita ohjelma, joka kysyy käyttäjältä nimiä siihen saakka, kunnes käyttäjä syöttää tyhjän merkkijonon.
+# Kunkin nimen syöttämisen jälkeen ohjelma tulostaa joko tekstin Uusi nimi tai Aiemmin syötetty nimi sen mukaan,
+# syötettiinkö nimi ensimmäistä kertaa. Lopuksi ohjelma luettelee syötetyt nimet yksi kerrallaan allekkain
+# mielivaltaisessa järjestyksessä. Käytä joukkotietorakennetta nimien tallentamiseen.
+
+nimi = input("Syötä nimi: ")
+nimet = {nimi}
+print("Uusi nimi.")
+nimet.add(nimi)
+
+while nimi != '':
+    nimi = input("Syötä nimi: ")
+    if nimi == '':
+        break
+    elif nimi not in nimet:
+        print("Uusi nimi.")
+    elif nimi in nimet:
+        print("Aiemmin syötetty nimi.")
+    nimet.add(nimi)
+
+print("Painoit 'enter' ja ohjelma lopetti toimintansa")
+print("Tulostetaan syötetyt nimet mielivaltaisessa järjestyksessä: ")
+for i in nimet:
+    print(i)
+
