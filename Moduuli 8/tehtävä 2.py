d46d6819b2d783037d92887959af679f01aaf8fa
elliriskala <elli.riskala@hotmail.com>
2023-09-21 13:58:40 +0300

    Moduuli 8

--- Moduuli 8/tehtävä 2.py
+++ Moduuli 8/tehtävä 2.py
@@ -1,7 +1,5 @@
-# Kirjoita ohjelma, joka kysyy käyttäjältä maakoodin (esimerkiksi FI)
-# ja tulostaa kyseisessä maassa olevien lentokenttien lukumäärät tyypeittäin.
-# Esimerkiksi Suomen osalta tuloksena on saatava tieto siitä, että pieniä
-# lentokenttiä on 65 kappaletta, helikopterikenttiä on 15 kappaletta jne.
+# Kirjoita ohjelma, joka kysyy käyttäjältä maakoodin (esimerkiksi FI) ja tulostaa kyseisessä maassa olevien lentokenttien lukumäärät tyypeittäin.
+# Esimerkiksi Suomen osalta tuloksena on saatava tieto siitä, että pieniä lentokenttiä on 65 kappaletta, helikopterikenttiä on 15 kappaletta jne.
 
 import mysql.connector
 
@@ -13,4 +11,33 @@ connection = mysql.connector.connect(
     password='koira12',
     autocommit=True
     )
+
+def get_airport_type_by_iso_country(iso_country, airport_type):
+    sql = f"select type from airport where airport.iso_country = '{iso_country}'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount <= 0:
+        print("No results. ")
+        return -1
+    for row in result:
+        if row[0] in airport_type:
+            airport_type[row[0]] += 1
+    print(f"Syöttämälläsi maakoodilla: '{iso_country}' saadut lentokentät: '{airport_type}'")
+    return
+
+
+airport_type = {'heliport': 0,
+                'small_airport': 0,
+                'medium_airport': 0,
+                'large_airport': 0,
+                'closed': 0
+}
+
+iso_country = input("Ohjelma tulostaa maakoodilla kyseisen maan lentokentät. Syötä haluamasi maakoodi: ").upper()
+get_airport_type_by_iso_country(iso_country, airport_type)
+
+
+
+
 
